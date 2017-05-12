f28f3afc4accc3c472572f8e479fecfb5fe45eab
Zack Scholl <zack.scholl@gmail.com>
2017-05-11 18:37:33 -0600

    testing

---
+++ test.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    raise
+
+try:
+    tshark = which("tshark")
+except:
+    print("tshark not found, you must install using\n\napt-get install tshark")
