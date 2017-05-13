f2b59a0818efbc7da584f82607775ed347b2d938
Zack Scholl <zack.scholl@gmail.com>
2017-05-13 10:46:48 -0600

    Update setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='howmanypeoplearearound',
     packages=['howmanypeoplearearound'],
-    version='0.1.0',
+    version='0.1.1',
     description='A tshark wrapper to count the number of cellphones in the vicinity',
     author='schollz',
     url='https://github.com/schollz/howmanypeoplearearound',
     author_email='hypercube.platforms@gmail.com',
-    download_url='https://github.com/schollz/howmanypeoplearearound/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/schollz/howmanypeoplearearound/archive/v0.1.1.tar.gz',
     keywords=['tshark', 'wifi', 'location'],
     classifiers=[],
     install_requires=[
